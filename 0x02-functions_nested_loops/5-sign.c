63dba88374ed1f85c0e35c9b01f8de35f6904708
edaoudihicham <edaoudihicham@gmail.com>
2023-06-20 19:43:29 -0700

    s

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,18 +8,20 @@
  * -1 and prints - if n is less than zero
  */
 int print_sign (int n)
-    if (n > 0)
-    {
-        _putchar(43);
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar(48);
-        return (0);
-    }
-    else
-    {
-        _putchar(45);
-        return (-1);
- }
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}

07480ba1d36c5573a9ff9ffa71e9d3aebd0819ac
“Hicham <edaoudihicham@gmail.com>
2023-10-31 16:07:04 -0700

    4fix2

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,29 +1,42 @@
-i#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * free_grid - frees 2d array
- * @grid: 2d grid
- * @height: height dimension of grid
- * Description: frees memory of grid
- * Return: nothing
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
+ * Return: pointer to 2 dim. array
  */
 
-void free_grid(int **grid, int height)
-
+int **alloc_grid(int width, int height)
 {
-	int i;
+	int **pointer;
+	int i, j;
 
-
-	for (i = 0; i < height; i++)
-
+	if (width <= 0 || height <= 0)/* A */
+		return (NULL);
+	if (width > INT_MAX || height > INT_MAX)
+		return (NULL);
+	pointer = malloc(height * sizeof(int *));/* D */
+	if (pointer == NULL)
 	{
-
-		free(grid[i]);
-
+		free(pointer);
+		return (NULL);
 	}
-
-	free(grid);
-
+	for (i = 0; i < height; i++)/* E */
+	{
+		pointer[i] = malloc(width * sizeof(int));
+		if (pointer[i] == NULL)
+		{
+			for ( ; i >= 0; i--)
+				free(pointer[i]);
+			free(pointer);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)/* F */
+		for (j = 0; j < width; j++)
+			pointer[i][j] = 0;
+	return (pointer);/* G */
 }
